f05fd1f1edfa3caffbd6c8513c25767e700eb329
user0032 <70772813+PCuser0032@users.noreply.github.com>
2022-03-31 11:14:22 +0300

    Rewrite MyGeometricFigure.cs

--- MyGeometricFigure.cs
+++ MyGeometricFigure.cs
@@ -25,18 +25,32 @@
         }
     }
 
-    public abstract class Polygon : MyGeometricFigure
-    {
-        public abstract double GetPerimeter();
-        public abstract void GetInfo();
-    }
-
-    public abstract class RegularPolygon : Polygon
+    class RegularPolygon : MyGeometricFigure
     {
         public double SideLength { get; set; }
+        double NumberOfSides;
+
+        public override double GetArea()
+        {
+            return NumberOfSides / 4.0 * Math.Pow(SideLength, 2.0) * 1.0 / Math.Tan(Math.PI / NumberOfSides);
+        }
+
+        public override double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+        public double GetInscribedCircleRadius()
+        {
+            return GetCircumscribedCircleRadius() * Math.Cos(Math.PI / NumberOfSides);
+        }
+
+        public double GetCircumscribedCircleRadius()
+        {
+            return SideLength * 1 / 2.0 * 1.0 / Math.Sin(Math.PI / NumberOfSides);
+        }
     }
 
-    class Triangle : Polygon
+    class Triangle : MyGeometricFigure
     {
         public double AB { get; set; }
         public double BC { get; set; }
@@ -62,17 +76,9 @@
         {
             return (AB * BC * CA) / (4.0 * GetArea());
         }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длины сторон: AB = { AB }, BC = { BC }, CA = { CA }");
-        }
     }
 
-    public class Quadrangle : Polygon
+    class Quadrangle : MyGeometricFigure
     {
         public double AB { get; set; }
         public double BC { get; set; }
@@ -91,18 +97,9 @@
         {
             return AB + BC + CD + DA;
         }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длины сторон: AB = { AB }, BC = { BC }, CD = { CD }, DA = { DA }");
-            Console.WriteLine($"Величины углов:  Alpha = { AngleAlpha }, Beta = { AngleBeta }");
-        }
     }
 
-    class Rectangle : Polygon
+    class Rectangle : MyGeometricFigure
     {
         public double Width { get; set; }
         public double Height { get; set; }
@@ -116,14 +113,6 @@
         {
             return 2.0 * (Height + Width);
         }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длины сторон: AB = CD = { Width }, BC = DA = { Height }");
-        }
     }
 
     class Trapeze : Quadrangle
@@ -134,15 +123,6 @@
         {
             return (AB + CD) / 2.0 * Height;
         }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длины сторон: AB = { AB }, BC = { BC }, CD = { CD }, DA = { DA }");
-            Console.WriteLine($"Высота трапеции: h = { Height }");
-        }
     }
 
     class Parallelogram : Quadrangle
@@ -151,15 +131,6 @@
         public override double GetArea()
         {
             return AB * DA * Math.Sin(Angle * Math.PI / 180.0);
-        }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длины сторон: AB = CD = { AB }, BC = DA = { DA }");
-            Console.WriteLine($"Угол между сторонами AB и DA: { Angle }");
         }
     }
 
@@ -174,14 +145,6 @@
         {
             return 4.0 * AB;
         }
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { AB }");
-            Console.WriteLine($"Угол между сторонами AB и DA: { Angle }");
-        }
     }
 
     class Square : RegularPolygon
@@ -195,198 +158,28 @@
         {
             return 4.0 * SideLength;
         }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
     }
 
     class Pentagon : RegularPolygon
     {
-        public override double GetArea()
-        {
-            return Math.Sqrt(5.0) * Math.Sqrt(5.0 + 2.0 * Math.Sqrt(5)) / 4.0 * Math.Pow(SideLength, 2.0);
-        }
-
-        public override double GetPerimeter()
-        {
-            return 5.0 * SideLength;
-        }
-
-        public double GetInscribedCircleRadius()
-        {
-            return Math.Sqrt(5.0) * Math.Sqrt(5.0 + 2.0 * Math.Sqrt(5)) / 10.0 * SideLength;
-        }
-
-        public double GetCircumscribedCircleRadius()
-        {
-            return Math.Sqrt(10.0) * Math.Sqrt(5.0 + Math.Sqrt(5)) / 10.0 * SideLength;
-        }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
     }
     class Hexagon : RegularPolygon
     {
-        public override double GetArea()
-        {
-            return 3.0 * Math.Sqrt(3) / 2.0 * Math.Pow(SideLength, 2.0);
-        }
-
-        public override double GetPerimeter()
-        {
-            return 6.0 * SideLength;
-        }
-
-        public double GetInscribedCircleRadius()
-        {
-            return Math.Sqrt(3) / 2.0 * SideLength;
-        }
-
-        public double GetCircumscribedCircleRadius()
-        {
-            return SideLength;
-        }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
     }
 
     class Heptagon : RegularPolygon
     {
-        public override double GetArea()
-        {
-            return 7.0 / 4.0 * Math.Pow(SideLength, 2.0) * 1.0 / Math.Tan(Math.PI / 7.0);
-        }
-
-        public override double GetPerimeter()
-        {
-            return 7.0 * SideLength;
-        }
-
-        public double GetInscribedCircleRadius()
-        {
-            return GetCircumscribedCircleRadius() * Math.Cos(Math.PI / 7.0);
-        }
-
-        public double GetCircumscribedCircleRadius()
-        {
-            return SideLength / (2.0 * Math.Sin(Math.PI / 7.0));
-        }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
     }
 
     class Octagon : RegularPolygon
     {
-        public override double GetArea()
-        {
-            return 2.0 * 1.0 / Math.Tan(Math.PI / 8.0) * Math.Pow(SideLength, 2.0);
-        }
-
-        public override double GetPerimeter()
-        {
-            return 8.0 * SideLength;
-        }
-
-        public double GetInscribedCircleRadius()
-        {
-            return (1.0 + Math.Sqrt(2.0)) / 2.0 * SideLength;
-        }
-
-        public double GetCircumscribedCircleRadius()
-        {
-            return SideLength * Math.Sqrt((1.0 + Math.Sqrt(2.0)) / ((1.0 + Math.Sqrt(2.0) - 1.0)));
-        }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
     }
 
     class Nonagon : RegularPolygon
     {
-        public override double GetArea()
-        {
-            return 9.0 / 4.0 * Math.Pow(SideLength, 2.0) * 1.0 / Math.Tan(Math.PI / 9.0);
-        }
-
-        public override double GetPerimeter()
-        {
-            return 9.0 * SideLength;
-        }
-
-        public double GetInscribedCircleRadius()
-        {
-            return GetCircumscribedCircleRadius() * Math.Cos(Math.PI / 9.0);
-        }
-
-        public double GetCircumscribedCircleRadius()
-        {
-            return SideLength / (2.0 * Math.Sin(Math.PI / 9.0));
-        }
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
-
     }
 
     class Decagon : RegularPolygon
     {
-        public override double GetArea()
-        {
-            return 5.0 / 2.0 * Math.Pow(SideLength, 2.0) * Math.Sqrt(5.0 + 2.0 * Math.Sqrt(5.0));
-        }
-
-        public override double GetPerimeter()
-        {
-            return 10.0 * SideLength;
-        }
-
-        public double GetInscribedCircleRadius()
-        {
-            return Math.Sqrt(5.0 + 2.0 * Math.Sqrt(5.0)) / 2.0 * SideLength;
-        }
-
-        public double GetCircumscribedCircleRadius()
-        {
-            return (Math.Sqrt(5.0) + 1.0) / 2.0 * SideLength;
-        }
-
-        public override void GetInfo()
-        {
-            Console.WriteLine($"Имя фигруры: { GetName() }");
-            Console.WriteLine($"Периметр: { GetPerimeter() }");
-            Console.WriteLine($"Площадь: { GetArea() }");
-            Console.WriteLine($"Длина стороны: { SideLength }");
-        }
     }
 }
